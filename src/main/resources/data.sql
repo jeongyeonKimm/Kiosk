776d57e6e0aee29965be2b7a7bfb34e383e2ea54
Jeongyeon Kim <wjddus9132@gmail.com>
2025-02-22 11:01:28 +0900

    feat: 더미 데이터 SQL 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO PRODUCT (product_number, name, price, product_type, selling_status)
+VALUES ('001', '아메리카노', 4000, 'HANDMADE', 'SELLING'),
+       ('002', '사과쥬스', 4500, 'BOTTLE', 'SOLD_OUT'),
+       ('003', '소금빵', 3500, 'HANDMADE', 'STOP_SELLING');
