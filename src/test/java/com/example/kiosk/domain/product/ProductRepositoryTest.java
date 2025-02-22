b8c3f4ded22636333c2ce60e302845e25a2e7e98
Jeongyeon Kim <wjddus9132@gmail.com>
2025-02-22 09:46:57 +0900

    test: 원하는 상품 타입, 판매 상태를 가진 상품 조회 테스트 작성

--- src/test/java/com/example/kiosk/domain/product/ProductRepositoryTest.java
+++ src/test/java/com/example/kiosk/domain/product/ProductRepositoryTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.util.List;
 
 import static com.example.kiosk.domain.product.ProductSellingStatus.*;
-import static com.example.kiosk.domain.product.ProductType.HANDMADE;
+import static com.example.kiosk.domain.product.ProductType.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
@@ -22,11 +22,11 @@ class ProductRepositoryTest {
 
     @DisplayName("전체 메뉴를 조회한다.")
     @Test
-    void getProducts() {
+    void findALl() {
         // given
         Product product1 = createProduct("001", "아메리카노", 4000, HANDMADE, SELLING);
         Product product2 = createProduct("002", "카페라떼", 4500, HANDMADE, SELLING);
-        Product product3 = createProduct("003", "소금빵", 3500, ProductType.BAKERY, SOLD_OUT);
+        Product product3 = createProduct("003", "소금빵", 3500, BAKERY, SOLD_OUT);
 
         productRepository.saveAll(List.of(product1, product2, product3));
 
@@ -39,7 +39,51 @@ class ProductRepositoryTest {
                 .containsExactlyInAnyOrder(
                         tuple("001", "아메리카노", 4000, HANDMADE, SELLING),
                         tuple("002", "카페라떼", 4500, HANDMADE, SELLING),
-                        tuple("003", "소금빵", 3500, ProductType.BAKERY, SOLD_OUT)
+                        tuple("003", "소금빵", 3500, BAKERY, SOLD_OUT)
+                );
+    }
+
+    @DisplayName("원하는 상품 타입에 해당하는 상품을 조회한다.")
+    @Test
+    void findAllByProductTypeIn() {
+        // given
+        Product product1 = createProduct("001", "아메리카노", 4000, HANDMADE, SELLING);
+        Product product2 = createProduct("002", "사과쥬스", 4500, BOTTLE, SELLING);
+        Product product3 = createProduct("003", "소금빵", 3500, BAKERY, SOLD_OUT);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllByProductTypeIn(List.of(HANDMADE, BAKERY));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "price", "productType", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", 4000, HANDMADE, SELLING),
+                        tuple("003", "소금빵", 3500, BAKERY, SOLD_OUT)
+                );
+    }
+
+    @DisplayName("원하는 판매 상태를 가진 상품을 조회한다.")
+    @Test
+    void findAllBySellingStatusIn() {
+        // given
+        Product product1 = createProduct("001", "아메리카노", 4000, HANDMADE, SELLING);
+        Product product2 = createProduct("002", "사과쥬스", 4500, BOTTLE, SOLD_OUT);
+        Product product3 = createProduct("003", "소금빵", 3500, BAKERY, STOP_SELLING);
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        // when
+        List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, SOLD_OUT));
+
+        // then
+        assertThat(products).hasSize(2)
+                .extracting("productNumber", "name", "price", "productType", "sellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "아메리카노", 4000, HANDMADE, SELLING),
+                        tuple("002", "사과쥬스", 4500, HANDMADE, SELLING)
                 );
     }
 
